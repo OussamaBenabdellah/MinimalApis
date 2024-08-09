8c78d79a91e664ce80e481617c5c9d8f0489566e
salim BENABDELLAH <salim.benabdellah@hotmail.fr>
2024-08-09 21:16:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿///
+/// WebApplication c'est une class 
+/// CreateBuilder une methode de WebApplication qui est static et son type retour est  WebApplicationBuilder
+/// 
+///
+
+var builder = WebApplication.CreateBuilder();
+///
+/// Build est une methode de notre variable builder et son type de retour WebApplication
+///
+var app = builder.Build();
+///
+/// les verbes HTTP
+///
+
+app.MapGet("/get", () => "Hello GET"); 
+
+app.MapDelete("/delete", () =>"Hello DELETE" );
+
+app.MapPost("/post", () => "Hello POST");
+
+app.MapPut("/put", () => "Hello PUT");
+
+app.MapPatch("/patch", () => "Hello PATCH");
+
+
+
+
+
+app.Run();
